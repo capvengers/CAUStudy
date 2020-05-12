142ba9ae812847e5c0102712d16e488f860e2215
jesn1219 <jesn1219@gmail.com>
2020-05-13 06:31:25 +0900

    count error updated, study list attach updated

--- app/src/main/java/com/example/caustudy/MainActivity.java
+++ app/src/main/java/com/example/caustudy/MainActivity.java
@@ -33,11 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_searchstudy, R.id.navigation_mystudy)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
