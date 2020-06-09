24baad55dd8a91a1efaaf377035bcbf602256b01
jesn1219 <jesn1219@gmail.com>
2020-06-09 15:57:41 +0900

    멤버관리 파일만 추가, UI 내꺼에 맞게 수정

---
+++ app/src/main/java/com/example/caustudy/Member_management.java
@@ -0,0 +1,14 @@
+package com.example.caustudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Member_management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_management);
+    }
+}
